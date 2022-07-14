7e2dddb00fad335f482c603c5a73a5173738b513
Ubuntu <ubuntu@ip-172-31-85-100.ec2.internal>
2022-07-14 11:55:05 +0000

    final commit

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=daredrexel/ml-api
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
+docker login -u daredrexel
+docker tag ml-api $dockerpath:latest
+
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
